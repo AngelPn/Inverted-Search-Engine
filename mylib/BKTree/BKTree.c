3cd7421f4c4435a09a68aab523bf939ed659258f
LtVaios <vaioslt123@gmail.com>
2021-10-22 12:44:59 +0300

    BKTREE.h initialise, BKTree structs developed

--- mylib/BKTree/BKTree.c
+++ mylib/BKTree/BKTree.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "BKTree.h"
 
-int compare_words(const char* word1, const char* word2);
+
+struct treenode{
+    char* word;
+    /* cost is the distance between the current word and the parent's one */
+    int cost;
+    /*Child is the list with all child nodes of the current node*/
+    BK_treenode child;
+    /* Next is the next element of parent node's children*/
+    BK_treenode next;
+};
+
+struct tree{
+    BK_treenode root;
+    /*Size is the total number of nodes*/
+    int size;
+};
 
 int compare_words(const char* word1, const char* word2){
     /* using edit distance */
---
+++ mylib/BKTree/BKTree.h
@@ -0,0 +1,8 @@
+#include "../../include/core.h"
+
+typedef struct treenode* BK_treenode;
+typedef struct tree* BK_tree;
+
+enum ErrorCode build_entry_index(const entry_ist* el, enum MatchType type, index* ix)
+int compare_words(const char* word1, const char* word2);
+enum ErrorCode destroy_entry_index(index* ix)
