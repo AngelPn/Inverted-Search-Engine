828ce4a2a69cb1d45aa387c7c659c9dee809a519
AngelPn <aggi.pa@gmail.com>
2021-10-30 16:40:38 +0300

    acutest setup

--- mylib/Entry/Entry.c
+++ mylib/Entry/Entry.c
@@ -39,3 +39,15 @@ ErrorCode destroy_entry(void *e){
     free(ce);
     return EC_SUCCESS;
 }
+
+
+void test_example(void) {
+    void* mem;
+
+    mem = malloc(10);
+    TEST_CHECK(mem != NULL);
+
+    mem = realloc(mem, 20);
+    TEST_CHECK(mem != NULL);
+
+}
