35ac25343883a2e0ec38abc39451015e96ec1ca7
LtVaios <vaioslt123@gmail.com>
2021-12-04 01:00:23 +0200

    destroy entry bugfix

--- mylib/Entry/Entry.c
+++ mylib/Entry/Entry.c
@@ -53,6 +53,12 @@ void print_entry_word(void *e){
 
 ErrorCode destroy_entry(void **e) {
     entry ce = *e;
+    for(int i=0 ; i<3 ; i++) {
+        if(ce->payload[i]) {
+            destroy_list(&(ce->payload[i]));
+            ce->payload[i] = NULL;
+        }
+    }
     free(ce->w);
     free(ce);
     ce = NULL;
